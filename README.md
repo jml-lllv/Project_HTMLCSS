94fdd13f6d786f32a5f8acc2a816f90fa3f7096f
Yiordi Andrei Murillo Levin <70813099+jml-lllv@users.noreply.github.com>
2021-02-25 19:21:01 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Project_Greko
+ Project in HTML, CSS, Javascript JQuery
---
+++ js/main.js
@@ -0,0 +1,14 @@
+//HEADER//
+/*$(document).ready(function(){
+ 
+	$(window).scroll(function(){
+		if( $(this).scrollTop() > 0 ){
+			$('#header').removeClass('cabecera');
+			$('#header').addClass('cabecera2');
+		} else {
+			$('#header').removeClass('cabecera2');
+			$('#header').addClass('cabecera');
+		}
+	});
+});
+*/
